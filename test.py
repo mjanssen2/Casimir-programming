f71e42f07588eb50b88da712ffd1137ff1afc88b
Martijn Janssen <m.t.g.janssen@tudelft.nl>
2019-11-06 13:20:23 +0100

    my second commit

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("Hello World")
+import numpy as np
+print("Hoi","lekkkkahh")
 
+def surface_area(r):
+	return np.pi*r**2
+
+def circumference(r):
+	return 2*np.pi*r
+
+print("Circumference of circle with radius 1: ", circumference(1))
+print("Surface area of circle with radius 1: ", surface_area(1))
